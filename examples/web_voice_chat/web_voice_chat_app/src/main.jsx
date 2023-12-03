073af29e0b74abf3630c10f07c2c9cc530da6378
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-12-03 17:49:34 +0000

    Removes browserrouter

--- examples/web_voice_chat/web_voice_chat_app/src/main.jsx
+++ examples/web_voice_chat/web_voice_chat_app/src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import BrowserRouter from "react-router-dom/BrowserRouter";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={"https://mriios.github.io/Parapet-Workbench/"}>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
